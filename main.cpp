78a012a28f4d4c748bfa33a54c9a3a05455d27d4
Jared Childers <jared.childers15@hotmail.com>
2016-04-18 20:25:32 -0700

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+string encode(string inp1, int inp2) {
+
+}
+
+string decypher(string inp1, int inp2) {
+	string temp(inp1);
+	for (int i = 0; i < temp.size(); i++) {
+		temp.at(i) = temp.at(i) +
+	}
+}
+
+int main() {
+
+	string input, input2;
+
+	cout << "Enter a message: " << endl;
+	getline(cin, input);
+
+	cout << "Enter the amount you want to decode: " << endl;
+	getline(cin, input2);
+
+
+
+	system("pause");
+	return 0;
+}
